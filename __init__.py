117de2fc4054a3d4b2531ffb28366b19fa29a9b5
meanin2 <113129684+meanin2@users.noreply.github.com>
2025-01-21 10:51:42 +0200

    Update __init__.py
    Adding cot processing node for ollama r1

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .nodes.image_watermark_node import ImageWatermarkNode
+from .nodes.chain_of_thought_processor_node import ChainOfThoughtProcessorNode
 
 NODE_CLASS_MAPPINGS = {
     "ImageWatermarkNode": ImageWatermarkNode,
+    "ChainOfThoughtProcessorNode": ChainOfThoughtProcessorNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageWatermarkNode": "Add Image Watermark",
+    "ChainOfThoughtProcessorNode": "Chain of Thought Processor"
 }
 
 __all__ = [
